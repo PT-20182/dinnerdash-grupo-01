cd23e06ff4f3e3e126f13be37f3c4cfbf0dd8c51
GabrielBessa <gabrielbessarofl@gmail.com>
2018-12-11 23:25:16 -0200

    Cart page nearly finished

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,7 @@
 class CartController < ApplicationController
+  # include ActionView::Helpers::UrlHelper
 
+  
   def add_item_to_cart
     @id = params[:id]
     @quantity = params[:quantity]
@@ -7,13 +9,21 @@ class CartController < ApplicationController
     redirect_to root_path
   end
   
-  def remove_item_from_cart
+  def remove_item_from_cart_main
     set_cart.delete_if {|x| x["id"] == params[:id]}
 
     redirect_to root_path
   end
 
+  def remove_item_from_cart_checkout
+    set_cart.delete_if {|x| x["id"] == params[:id]}
 
+    redirect_to cart_path
+  end
+
+  def index
+    @meals = Meal.all
+  end
 
 private
 
